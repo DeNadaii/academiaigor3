e520103743838efc3e81ea22dea0202ee9122eb8
Igor <Igor@DESKTOP-QI5I0NM>
2021-11-26 13:34:59 -0300

    Adicionar arquivos de projeto.

---
+++ academiaigor3/Models/EmpresaModels.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AcademiaIgor.Models
+{
+    public class EmpresaModels
+    {
+        [Key]
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public string NomeFantasia { get; set; }
+        public int Cnpj { get; set; }
+
+    }
+}
